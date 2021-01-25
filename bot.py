27afdd746333da38485fa5d199c81a143b3a20ab
Kai-Builder <gkai70264@gmail.com>
2021-01-25 11:52:40 -0500

    fix music bot abilities

--- bot.py
+++ bot.py
@@ -6,13 +6,41 @@ from discord import channel
 from discord.ext import commands
 import random
 import json
-
+import logging
+import math
+from urllib import request
 from datetime import datetime
 import asyncio
+import youtube_dl as ytdl
 from discord.utils import find, get
 from pathlib import Path
 
 client = commands.Bot(command_prefix="p:")
+
+
+YTDL_OPTS = {
+    "default_search": "ytsearch",
+    "format": "bestaudio/best",
+    "quiet": True,
+    "extract_flat": "in_playlist"
+}
+
+
+def _play_song(self, client, state, song):
+    state.now_playing = song
+    state.skip_votes = set()  # clear skip votes
+    source = discord.PCMVolumeTransformer(
+        discord.FFmpegPCMAudio(song.stream_url), volume=state.volume)
+
+    def after_playing(err):
+        if len(state.playlist) > 0:
+            next_song = state.playlist.pop(0)
+            self._play_song(client, state, next_song)
+        else:
+            asyncio.run_coroutine_threadsafe(client.disconnect(),
+                                             self.bot.loop)
+
+    client.play(source, after=after_playing)
 
 
 async def status_task():
@@ -45,6 +73,15 @@ async def register(ctx, name, dob, age, pw):
     i = open(name + "/user.txt", 'w')
     i.write(name + "\n" + age + "\n" + dob + "\n" + pw)
     i.close()
+
+
+async def audio_playing(ctx):
+    """Checks that audio is currently playing before continuing."""
+    client = ctx.guild.voice_client
+    if client and client.channel and client.source:
+        return True
+    else:
+        raise commands.CommandError("Not currently playing any audio.")
 
 
 @client.event
@@ -267,47 +304,7 @@ async def ban(ctx, user: discord.Member, *, reason):
     await ctx.send(f"Banned @{user} With Reason Of {reason}")
 
 
-@client.command()
-async def play(ctx, url: str):
-    global name
-    song_there = os.path.isfile('song.mp3')
-    try:
-        if song_there:
-            os.remove('song.mp3')
-            print('Removed current song.')
-    except PermissionError:
-        print('Error in deleting song file. (Song in use.)')
-        await ctx.send('Unable to request song. (Song already in use.)')
-        return
 
-    await ctx.send('Preparing song. Please wait.')
-    voice = get(client.voice_clients, guild=ctx.guild)
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        print('Downloading audio now.\n')
-        ydl.download([url])
-
-    for file in os.listdir('./'):
-        if file.endswith('.mp3'):
-            name = file
-            print(f'Renamed File: {file}.')
-            os.rename(file, 'song.mp3')
-
-    voice.play(discord.FFmpegPCMAudio('song.mp3'), after=lambda e: print(f'{name} has finished playing.'))
-    voice.source = discord.PCMVolumeTransformer(voice.source)
-    voice.source.volume = 0.06
-
-    nname = name.rsplit('-', 2)
-    await ctx.send(f'Now playing {name}.')
-    print('Now playing.\n')
 
 
 @client.command()
@@ -340,17 +337,15 @@ async def help(ctx, command=None):
     print("heloin")
     if command is None:
         print("nonecomd")
-        await ctx.send("Commands"
+        await ctx.send("Default Commands. Say p:help <utilpack> For More Info On that Specific Package."
                        "\n"
                        "\n p:help, p:post, p:play, p:leave, p:submit, p:submitsong, p:marketplace, p:subforum <name>")
     else:
-        a = open('doc/' + command, "r+")
-        i = a.readlines()
-        a = ''.join(i)
-        embe = discord.Embed(title=command, description=f"This is the Utility's {command} Read me.")
-        await ctx.send(embed=embe)
-        for line in a:
-            await ctx.send(line)
+        if command == "community":
+            embed = discord.Embed(title="Community Commands", description="All Of the Basic Community Utilities")
+            embed.add_field(name="Networks",
+                            value="p:post\np:top_posts\np:view <post>\np:submit <util>\np:submitsong <url>")
+            await ctx.send(embed=embed)
 
 
 @client.command()
@@ -383,6 +378,17 @@ async def publicparties(ctx):
             print("PrivateLobby")
 
 
+async def in_voice_channel(ctx):
+    """Checks that the command sender is in the same voice channel as the bot."""
+    voice = ctx.author.voice
+    bot_voice = ctx.guild.voice_client
+    if voice and bot_voice and voice.channel and bot_voice.channel and voice.channel == bot_voice.channel:
+        return True
+    else:
+        raise commands.CommandError(
+            "You need to be in the channel to do that.")
+
+
 @client.command()
 async def joinparty(ctx, code: int, *, name):
     print("joinparty Called")
@@ -398,44 +404,177 @@ async def joinparty(ctx, code: int, *, name):
 sent_users = []
 
 
-@client.event
-async def on_message(message):
-
-    if message.guild:  # ensure the channel is a DM
-        return
-
-    if message.author == client.user:
-        return
-
-    if message.author.id in sent_users:  # Ensure the intial message hasn't been sent before
-        return
-
-    modmail_channel = client.get_channel(803061426050170880)
-    await modmail_channel.send("User Logged, Started MODMAIL")
-    a = discord.Embed(title="ModMail")
-    a.set_author(name="Mailer")
-    a.add_field(name="How Does ModMail Work?",
-                    value="Modmail Is a Discord Bot That Eases The Process Of Sending DMs To Mods Using Tokens and Stuff. I Don't understand Either."
-                    )
-    a.add_field(name="Mailing Commands",
-                    value="mailto, mailtomods, joinmodparty")
-    a.add_field(name="Shoutouts",
-                    value="DKradicat, Kinetic_Unicornz, Echo_, Brop, USB (me) (Obviously), And All of my Other friends!")
-    await message.author.send(embed=a)
-
-    def check(m):
-            if m.content == "mailtomods":
-                sessionid = random.randint(0, 100002112312)
+@client.command()
+async def mailtomods(ctx, arg):
+    if isinstance(ctx.channel, discord.channel.DMChannel):
+        await ctx.send("Alright")
+        code = random.randint(0, 12073691093281287983)
+        mailchannel = client.get_channel(803061426050170880)
+        await ctx.send(f"Started Session. Code: {code}.")
+        await mailchannel.send(f"Started Session. Code: {code}.")
 
 
-                await message.author.send(f"Starting Your Session With Id, {sessionid}")
-                await modmail_channel.send(f"SessionID, {sessionid} Has Started.")
-
-    msg = await client.wait_for('message', check=check)
+@client.command(aliases=["stop"])
+@commands.guild_only()
+@commands.has_permissions(administrator=True)
+async def leave(self, ctx):
+    """Leaves the voice channel, if currently in one."""
+    clienst = ctx.guild.voice_client
+    state = self.get_state(ctx.guild)
+    if client and clienst.channel:
+        await clienst.disconnect()
+        state.playlist = []
+        state.now_playing = None
+    else:
+        await ctx.send("NOT IN A VOICE!!!")
 
 
 @client.command()
-async def check(ctx, arg):
-    if isinstance(ctx.channel, discord.channel.DMChannel):
-        await ctx.send(arg)
-client.run("TOKEN")
+@commands.check(audio_playing)
+@commands.check(in_voice_channel)
+async def skip(self, ctx):
+    """Skips the currently playing song, or votes to skip it."""
+    state = self.get_state(ctx.guild)
+    client = ctx.guild.voice_client
+    if ctx.channel.permissions_for(
+            ctx.author).administrator or state.is_requester(ctx.author):
+        # immediately skip if requester or admin
+        client.stop()
+    elif self.config["vote_skip"]:
+        # vote to skip song
+        channel = client.channel
+        self._vote_skip(channel, ctx.author)
+        # announce vote
+        users_in_channel = len([
+            member for member in channel.members if not member.bot
+        ])  # don't count bots
+        required_votes = math.ceil(
+            self.config["vote_skip_ratio"] * users_in_channel)
+        await ctx.send(
+            f"{ctx.author.mention} voted to skip ({len(state.skip_votes)}/{required_votes} votes)"
+        )
+    else:
+        raise commands.CommandError("Sorry, vote skipping is disabled.")
+
+
+class Video:
+    """Class containing information about a particular video."""
+
+    def __init__(self, url_or_search, requested_by):
+        """Plays audio from (or searches for) a URL."""
+        with ytdl.YoutubeDL(YTDL_OPTS) as ydl:
+            video = self._get_info(url_or_search)
+            video_format = video["formats"][0]
+            self.stream_url = video_format["url"]
+            self.video_url = video["webpage_url"]
+            self.title = video["title"]
+            self.uploader = video["uploader"] if "uploader" in video else ""
+            self.thumbnail = video[
+                "thumbnail"] if "thumbnail" in video else None
+            self.requested_by = requested_by
+
+    def _get_info(self, video_url):
+        with ytdl.YoutubeDL(YTDL_OPTS) as ydl:
+            info = ydl.extract_info(video_url, download=False)
+            video = None
+            if "_type" in info and info["_type"] == "playlist":
+                return self._get_info(
+                    info["entries"][0]["url"])  # get info for first video
+            else:
+                video = info
+            return video
+
+    def get_embed(self):
+        """Makes an embed out of this Video's information."""
+        embed = discord.Embed(
+            title=self.title, description=self.uploader, url=self.video_url)
+        embed.set_footer(
+            text=f"Requested by {self.requested_by.name}",
+            icon_url=self.requested_by.avatar_url)
+        if self.thumbnail:
+            embed.set_thumbnail(url=self.thumbnail)
+        return embed
+
+
+@client.command(brief="Plays audio from <url>.")
+async def play(self, ctx, *, url):
+    """Plays audio hosted at <url> (or performs a search for <url> and plays the first result)."""
+
+    clients = client.guild.voice_client
+    state = clients.get_state(ctx.guild)  # get the guild's state
+
+    if clients and clients.channel:
+        try:
+            video = Video(url, ctx.author)
+        except youtube_dl.DownloadError as e:
+            logging.warn(f"Error downloading video: {e}")
+            await ctx.send(
+                "There was an error downloading your video, sorry.")
+            return
+        state.playlist.append(video)
+        message = await ctx.send(
+            "Added to queue.", embed=video.get_embed())
+        await self._add_reaction_controls(message)
+    else:
+        if ctx.author.voice is not None and ctx.author.voice.channel is not None:
+            channel = ctx.author.voice.channel
+            try:
+                video = Video(url, ctx.author)
+            except youtube_dl.DownloadError as e:
+                await ctx.send(
+                    "There was an error downloading your video, sorry.")
+                return
+            client = await channel.connect()
+            self._play_song(client, state, video)
+            message = await ctx.send("", embed=video.get_embed())
+            await self._add_reaction_controls(message)
+            logging.info(f"Now playing '{video.title}'")
+        else:
+            await ctx.send("Failed To Play Video.")
+
+
+async def on_reaction_add(self, reaction, user):
+    """Respods to reactions added to the bot's messages, allowing reactions to control playback."""
+    message = reaction.message
+    if user != self.bot.user and message.author == self.bot.user:
+        await message.remove_reaction(reaction, user)
+        if message.guild and message.guild.voice_client:
+            user_in_channel = user.voice and user.voice.channel and user.voice.channel == message.guild.voice_client.channel
+            permissions = message.channel.permissions_for(user)
+            guild = message.guild
+            state = self.get_state(guild)
+            if permissions.administrator or (
+                    user_in_channel and state.is_requester(user)):
+                client = message.guild.voice_client
+                if reaction.emoji == "⏯":
+                    # pause audio
+                    self._pause_audio(client)
+                elif reaction.emoji == "⏭":
+                    # skip audio
+                    client.stop()
+                elif reaction.emoji == "⏮":
+                    state.playlist.insert(
+                        0, state.now_playing
+                    )  # insert current song at beginning of playlist
+                    client.stop()  # skip ahead
+            elif reaction.emoji == "⏭" and self.config[
+                "vote_skip"] and user_in_channel and message.guild.voice_client and message.guild.voice_client.channel:
+                # ensure that skip was pressed, that vote skipping is
+                # enabled, the user is in the channel, and that the bot is
+                # in a voice channel
+                voice_channel = message.guild.voice_client.channel
+                self._vote_skip(voice_channel, user)
+                # announce vote
+                channel = message.channel
+                users_in_channel = len([
+                    member for member in voice_channel.members
+                    if not member.bot
+                ])  # don't count bots
+                required_votes = math.ceil(
+                    self.config["vote_skip_ratio"] * users_in_channel)
+                await channel.send(
+                    f"{user.mention} voted to skip ({len(state.skip_votes)}/{required_votes} votes)"
+                )
+
+
+client.run("ODAyMTg5NzQ4MTIyNjgxMzU2.YArnaw.WvwS4twsA1ndvrY_iM-y0hiy9DY")
---
+++ readme.md
@@ -0,0 +1,39 @@
+<center style="font-size: xxx-large">DataBase Bot</center>
+<center style="font-size: small">The Fully Server-Friendly Bot For Discord.</center>
+
+
+
+The DataBase Bot is a Open-Source Discord Bot Used For
+- Account Management
+
+- Verification
+
+- Community Extensions
+
+- Playing Music
+
+- Working With Modmail
+
+- And Many more Features!
+
+DataBase Is Free to Use Under the MIT License (Cant See It Here).
+
+When Using, Apply your Token To your Discord Developer Application And Run it.
+
+Then, Your Bot Will Be Run With DataBase's Features.
+
+# Extensions
+Extensions Are Made For Bot Extensibility With Fun Packs Users Can Try Out. GUILD SPECIFIC!
+
+Extensions Can Be submitted With the p:submit Command!
+
+# Community
+Community Parties And Posts Are Available To the Public.
+
+This Bot Is Separatly Hosted So All Posts Are available On the Database.
+
+When you host yours, All YOUR Accounts will be available on your Machine/Database.
+
+# Commands Library
+
+###
